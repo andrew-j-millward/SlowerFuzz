d1deb9182eca5b6c47383fdc0ceae78716c75e28
andrew-j-millward <andrew.j.millward@gmail.com>
2021-11-17 15:27:05 -0500

    Added import

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ sys.path.append('../FTS')
 sys.path.append('../fuzzing')
 sys.path.append('../slowfuzz')
 sys.path.append('../woff')
+import argparse
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
 		description='This script optimizes evolutionary fuzzing by introducing structured randomness and eliminating inefficient paths early on.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-d', '--depth', type=int, metavar='', help='Number of elimination rounds')
 	parser.add_argument('-t', '--time', type=int, metavar='', help='Maximum time per exploration before analyzing the results',
-						default=10
+						default=10)
 
 	args = parser.parse_args()
 
-	
