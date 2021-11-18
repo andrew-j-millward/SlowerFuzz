0d1226d27716c36ddd44004b00698da45a575e6f
andrew-j-millward <andrew.j.millward@gmail.com>
2021-11-17 22:46:18 -0500

    Added params

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def initializeEnv(name):
 		print('Environment already set up... Continuing...')
 
 def runTest(name, timeout_period):
-	subprocess = Popen(command, stdout=PIPE, stderr=PIPE)
+	subprocess = Popen(['./runLibFuzzer.sh', name], stdout=PIPE, stderr=PIPE)
 	for i in range(timeout_period):
 		sleep(1)
 		if p.poll() is not None:
