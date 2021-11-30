41feb3e63ceed43273d1259191d85cb07603e7c6
andrew-j-millward <andrew.j.millward@gmail.com>
2021-11-30 12:34:32 -0500

    Debugging info

--- src/main.py
+++ src/main.py
@@ -33,6 +33,7 @@ def runLibFuzzer(name, timeout_period, seeds=[1], verbose=False):
 			if 'rss: ' in output[-j-1]:
 				parsed1 = output[-j-1].split('rss: ')
 				parsed2 = parsed1[1].split('Mb L:')
+				print(parsed1, parsed2)
 				memory[seeds[i]] = int(parsed2[0])
 				break
 	if verbose:
