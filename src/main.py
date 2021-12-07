dad28fda6c811aa3205c7feb396e6b47fe7f340b
andrew-j-millward <andrew-j-millward@gmail.com>
2021-12-06 19:47:07 -0500

    Added comments for main.py

--- src/main.py
+++ src/main.py
@@ -106,54 +106,100 @@ def runOptimization(depth, path, time, seeds, range_dict, libfuzzer, verbose=Fal
 
 #methods for slowFuzz
 def runSlowFuzz(name, timeout_period, seeds=[1], verbose=False):
+	"""
+	Method to run SlowFuzz on a given set of seeds for a specific number of iterations (timeout_period).
+	The run statistics will be aggregated and output in terms of slowdown since those are
+	the statistics of interest for SlowFuzz.
+	"""
+
 	slowdown = {}
+	
+	# Iterate over every seed
 	for i in range(len(seeds)):
+
+		# Create subprocess to execute the SlowFuzz driver with the given parameters
 		output = run("""
 				./driver corpus -artifact_prefix=out -print_final_stats=1 \
 				-detect_leaks=0 -rss_limit_mb=10000 -shuffle=0 \
 				-runs={0} -max_len=64 -death_node=1 \
 				-seed={1}
 				""".format(timeout_period, seeds[i]), stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
+
+		# Split output by line
 		output = output.stderr.split('\n')
+
+		# Scan through output and find last time
 		for j in range(len(output)):
 			if 'slowest_unit_time_sec:' in output[-j - 1]:
 				parsed1 = output[-j - 1].split('slowest_unit_time_sec: ')
 				slowdown[seeds[i]] = int(parsed1[1])
 				break
+
 	if verbose:
 		print(slowdown)
 	return slowdown
 
 def refineSeeds(range_dict, coverage):
+	"""
+	Method for taking coverage statistics and their respective seeds and applicable ranges and then
+	generate a new list of seeds to use for the next iteration. 
+	"""
+
+	# Sort seeds by coverage; take best 5.
 	optimal_seeds = sorted(coverage, key=coverage.get)[-5:]
+
+	# Variables
 	new_seeds = []
 	new_range_dict = {}
+
+	# For each of the optimal seeds, generate 5 more seeds within their respective domains
 	for i in range(5):
 		tmp_range_block = []
 		if optimal_seeds[i] not in range_dict: continue
+
+		# Grab bounds
 		lower = range_dict[optimal_seeds[i]][0]
 		upper = range_dict[optimal_seeds[i]][1]
+		
+		# Generate 5 new seeds between lower and upper
 		for j in range(5):
 			new_seeds.append(random.randint(lower, upper))
 			tmp_range_block.append(new_seeds[-1])
+
+		# Compute new ranges
 		tmp_range_block = sorted(tmp_range_block)
 		seed_ranges = [(lower, min(math.ceil((tmp_range_block[0] + tmp_range_block[1]) / 2), upper))]
 		for j in range(1, 4):
 			seed_ranges.append((max(lower, seed_ranges[-1][1] + 1),
 								min(math.ceil((tmp_range_block[j] + tmp_range_block[j + 1]) / 2), upper)))
 		seed_ranges.append((seed_ranges[-1][1] + 1, upper))
+
+		# Format in dict
 		for i in range(len(tmp_range_block)):
 			new_range_dict[tmp_range_block[i]] = seed_ranges[i]
+
 	return new_seeds, new_range_dict
 
 def initializeSeeds(seeds):
+	"""
+	Method to initialize the initial seeds across the total bounds. Returns the list of seeds, the ranges
+	that those seeds occupy (i'th seed occupies range of midpoint of i and i-1 and midpoint of i and i+1),
+	and a dictionary of these ranges.
+	"""
+
+	# Generate the seeds
 	seeds = sorted([random.randint(1, 9999999999) for i in range(seeds)])
+
+	# Compute the ranges according to the metric
 	seed_ranges = [(1, min(math.ceil((seeds[0] + seeds[1]) / 2), 9999999999))]
 	for i in range(1, len(seeds) - 1):
 		seed_ranges.append(
 			(max(1, seed_ranges[i - 1][1] + 1), min(math.ceil((seeds[i] + seeds[i + 1]) / 2), 9999999999)))
 	seed_ranges.append((seed_ranges[-1][1] + 1, 9999999999))
+
+	# Create dictionary
 	range_dict = {seeds[i]: seed_ranges[i] for i in range(len(seeds))}
+
 	return seeds, seed_ranges, range_dict
 
 if __name__ == '__main__':
@@ -203,21 +249,29 @@ if __name__ == '__main__':
 				 'vorbis-2017-12-11', 'woff2-2016-05-06', 'wpantund-2018-02-27']
 		debug_test = ['woff']
 
-		# Initialize and Run
+		# Clean up environment if needed
 		if args.path == 'clean':
 			if args.verbose:
 				print('Reset branch triggered... Removing all testing environments')
 			for i in range(len(tests)):
 				if os.path.isdir('../' + tests[i] + '_tmp'):
 					shutil.rmtree('../' + tests[i] + '_tmp')
+
+		# Run test or debug test case
 		elif args.path in tests or args.path in debug_test:
+
+			# Initialize test
 			if args.path in tests:
 				initializeEnv(args.path)
+
+			# Optimize seeds
 			optimal_seed, coverage_records = runOptimization(args.depth, args.path, args.time, seeds,
 						range_dict, args.libfuzzer, verbose=args.verbose)
 			if args.verbose:
 				print("Optimal seed {0} obtained, yielding coverage {1} after {2} iterations.".format(optimal_seed,
 						coverage_records[optimal_seed], args.time))
+
+			# Run optimal to completion
 			coverage, memory = runLibFuzzer(args.path, args.explorationdepth, seeds=[optimal_seed],
 						verbose=args.verbose)
 			if args.verbose:
@@ -233,9 +287,13 @@ if __name__ == '__main__':
 		os.chdir('../slowfuzz/apps/{0}/'.format(args.build))
 		os.system('make fuzzer')
 		os.system('make')
+
+		# Run optimization
 		optimal_seed, slowdown_records = runOptimization(args.depth, args.path, args.time, seeds, range_dict, args.libfuzzer, verbose=args.verbose)
 		if args.verbose:
 			print("Optimal seed {0} obtained, yielding slowdown {1} after {2} iterations.".format(optimal_seed, slowdown_records[optimal_seed], args.time))
+		
+		# Run full length
 		slowdown = runSlowFuzz(args.path, args.explorationdepth, seeds=[optimal_seed],verbose=args.verbose)
 		if args.verbose:
 			print(
